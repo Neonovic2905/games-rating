02c0a538b4adf70809b79bb05185da8c79748e74
Neonovic2905 <neon7582@gmail.com>
2024-05-02 19:45:03 +0300

    Исправление 3

--- controllers/game.js
+++ controllers/game.js
@@ -3,16 +3,19 @@ const {config}= require("../appModules/rating");
 const { getRandomGame } = require("../appModules/api");
 
 async function gameRouteController(res) {
-  try {
-    const ratingFile = await fs.readFile(config.PATH_TO_RATING_FILE, (error, data));
-    const data = JSON.parse(ratingFile);
-    const game = getRandomGame(data); // Получаем случайную игру
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(game));
-  } catch (error) {
-    console.log(error);
+
+  fs.readFile(config.PATH_TO_RATING_FILE, (err, data)=> {
+ if (err) {
+    console.log(err);
     res.statusCode = 500;
     res.end("Internal Server Error");
   }
+    const ratingFile = JSON.parse(data);
+    const game = getRandomGame(ratingFile); // Получаем случайную игру
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(game));
+
+  });
+
 }  
   module.exports = gameRouteController; 
